3519fea873a7e44d8ae8fb5e60bcfcde7bf057a0
Hennadii Shevchenko <shege68@gmail.com>
2023-05-23 18:26:51 +0300

    створив компонент header

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
+const Header = () => {
+    return <h1 className="header">HEADER</h1>
+}
+
 const App = () => {
-    return <></>
+    return (
+        <>
+            <Header />
+        </>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
